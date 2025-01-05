74ea7d74d061b0744fcc229c51e80b7b185a0fa4
pdesai-github <97328780+pdesai-github@users.noreply.github.com>
2025-01-05 22:45:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,5 +19,5 @@ def get_label_from_feature() -> str:
     return label;
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
